1d060785f44200901813ab80766d59f1b6ec4d38
cezar-pereira <helaniocezar@gmail.com>
2020-03-04 16:53:44 -0300

    removido o widget profile e adicionado o appBar e campo de pesquisa

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -2,19 +2,74 @@ import 'package:flutter/material.dart';
 import 'package:todolist/app/modules/home/Components.dart';
 import 'package:todolist/app/modules/home/home_controller.dart';
 import 'package:todolist/app/modules/home/home_module.dart';
-import 'package:todolist/app/modules/home/profile/profile_widget.dart';
 
 class HomePage extends StatelessWidget with Components {
   @override
   Widget build(BuildContext context) {
     HomeController homeController = HomeModule.to.get();
     return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+          child: Icon(Icons.settings, size: 28),
+        ),
+        title: ShaderMask(
+          shaderCallback: (rect) {
+            return RadialGradient(
+              center: Alignment.bottomCenter,
+              radius: 3,
+              colors: <Color>[Colors.white, Colors.cyan.withOpacity(0.6)],
+            ).createShader(rect);
+          },
+          child: Text("Lembre-me", style: TextStyle(fontSize: 25)),
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: CircleAvatar(
+              radius: 22,
+              backgroundColor: Colors.white30,
+              child: Icon(Icons.person),
+            ),
+          )
+        ],
+      ),
       body: SafeArea(
         child: Container(
-          padding: EdgeInsets.symmetric(vertical: 16, horizontal: 12),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              ProfileWidget(),
+              // ProfileWidget(),
+              Container(
+                padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(height: 12),
+                    Text(
+                      "Olá Cézar",
+                      style: TextStyle(fontSize: 23),
+                    ),
+                    SizedBox(height: 12),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      decoration: BoxDecoration(
+                        color: Colors.black12,
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                      child: TextField(
+                        maxLines: 1,
+                        decoration: InputDecoration(
+                          contentPadding:
+                              EdgeInsets.symmetric(horizontal: 8, vertical: 14),
+                          suffixIcon: Icon(Icons.search),
+                          border: InputBorder.none,
+                          hintText: "Procurar atividades",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
@@ -23,15 +78,17 @@ class HomePage extends StatelessWidget with Components {
                   period(text: "Mês"),
                 ],
               ),
+              SizedBox(height: 16),
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(22),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
                   child: GridView.count(
                     crossAxisSpacing: 12,
                     mainAxisSpacing: 12,
                     crossAxisCount: 2,
-                    children: List.generate(7, (index) {
-                      return categories(title: "Home", icon: Icons.home, amount: 10);
+                    children: List.generate(5, (index) {
+                      return categories(
+                          title: "Home", icon: Icons.home, amount: 10);
                     }),
                   ),
                 ),
